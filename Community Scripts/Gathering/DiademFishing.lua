8973712964805dcd16ec1f839e822142ca018c85
UcanPatates <56081921+UcanPatatess@users.noreply.github.com>
2024-05-22 23:21:55 +0300

    Update DiademFishing.lua

--- Community Scripts/Gathering/DiademFishing.lua
+++ Community Scripts/Gathering/DiademFishing.lua
@@ -7,9 +7,13 @@
     Author: UcanPatates  
 
     **********************
-    * Version  |  1.0.5  *
+    * Version  |  1.0.9  *
     **********************
 
+    -> 1.0.9  : Now purchases the exact amount of bait and dark matter.
+    -> 1.0.8  : Some tweaks to the pathing to fix a jumping when not supposed to issue.
+    -> 1.0.7  : 500 Cast and Super amiss check fixes.
+    -> 1.0.6  : Added NPC repair option and the ability to set a minimum number of baits to buy and Auto Cordial usage.
     -> 1.0.5  : Buying fix for user error.
     -> 1.0.4  : Now supports bait and Dark Matter buying, automatically selects the bait, and sets the AutoHook preset automatically.
     -> 1.0.3  : Now checks if the nvavmesh is ready.
@@ -24,6 +28,7 @@
     ***************
 
     This script will fish for you at diadem (Sweetfish).
+    
     here is a wiki:
     https://github.com/PunishXIV/AutoHook/wiki/The-Diadem
 
@@ -35,7 +40,7 @@
     -> SomethingNeedDoing (Expanded Edition) [Make sure to press the lua button when you import this] -> https://puni.sh/api/repository/croizat
     -> AutoHook : https://love.puni.sh/ment.json
     -> vnavmesh : https://puni.sh/api/repository/veyn
-    -> Pandora's Box -> https://love.puni.sh/ment.json
+    -> Pandora's Box : https://love.puni.sh/ment.json
     -> Simple Tweaks  
     
     **************
@@ -45,10 +50,17 @@
 
 --Do you want to buy Diadem Hoverworm and DarkMatter if you don't have it ?
 BuyBait = true           -- true | false (default is true)
+MinimumBait = 200        -- Minimum number of Baits you want.
+
 BuyDarkMatter = true     -- true | false (default is true)
+MinimumDarkMatter = 99   -- Minimum number of DarkMatter you want.
+
+--Auto use Cordial this is a pandora setting.
+UseCordial = true        -- true | false (default is true)
 
 -- When do you want to repair your own gear? From 0-100.
 SelfRepair = true        -- true | false (default is true)
+NpcRepair = false        -- true | false (default is false)
 StopTheScripIfThereIsNoDarkMatter = false -- true | false (default is false)
 RepairAmount = 99        -- Number between 1 to 99 
 
@@ -87,7 +99,7 @@ FishingSpot =
   {536.9,192.2,-503.2, 1},
   {570.3,189.4,-502.7, 1},
 
-  {455.9,255.3,535.1, 500} --bailout
+  {422.9,-191.2,-300.2, 500} --bailout
 }
 
 -- Functions
@@ -102,6 +114,51 @@ function unsetPropertyIfSet(propertyName)
     if GetSNDProperty(propertyName) then
         SetSNDProperty(propertyName, "false")
         LogInfo("[SetSNDPropertys] " .. propertyName .. " set to False")
+    end
+end
+
+function BuyPcall(ItemID, NpcName, ShopAddonName, SelectIconString, WhichSlotToBuy, TotalAmount)
+    local PurchaseLimit = 99
+    local FullBatches = TotalAmount / PurchaseLimit
+    local RemainingItems = TotalAmount % PurchaseLimit
+    local ItemCount = GetItemCount(ItemID)
+
+
+    local function PerformPurchase(Slot, Amount)
+        local ExpectedItemCount = ItemCount + Amount
+        while true do
+            yield("/wait 0.1")
+            ItemCount = GetItemCount(ItemID)
+            if GetTargetName() ~= NpcName then
+                yield("/target " .. NpcName)
+            elseif IsAddonVisible("SelectIconString") then
+                yield("/pcall SelectIconString true "..SelectIconString)
+            elseif IsAddonVisible("SelectYesno") then
+                yield("/pcall SelectYesno true 0")
+                yield("/wait 0.1")
+            elseif ItemCount  >= ExpectedItemCount then
+                break --Exit the loop
+            elseif IsAddonVisible(ShopAddonName) then
+                yield("/pcall " .. ShopAddonName .. " true 0 " .. Slot .. " " .. Amount)
+                yield("/wait 0.1")
+            else
+                yield("/interact")
+            end
+        end
+    end
+
+    for i = 1, FullBatches do
+        PerformPurchase(WhichSlotToBuy, PurchaseLimit)
+        yield("/wait 0.1")
+    end
+
+    if RemainingItems > 0 then
+        PerformPurchase(WhichSlotToBuy, RemainingItems)
+    end
+
+    while IsAddonVisible(ShopAddonName) do
+        yield("/pcall " .. ShopAddonName .. " true -1")
+        yield("/wait 0.1")
     end
 end
 
@@ -131,8 +188,10 @@ function LetsBuySomeStuff()
         local DiademHoverwormCount = GetItemCount(30281)
         local Grade8DarkMatterCount = GetItemCount(33916)
         local distance = GetDistanceToPoint(-641.2, 285.3, -138.7)
+        local BuyBaitMinimum = MinimumBait / 4
+        local BuyDarkMatterMinimum = MinimumDarkMatter / 4
 
-        if DiademHoverwormCount < 99 and BuyBait then
+        if DiademHoverwormCount < BuyBaitMinimum and BuyBait then
             if distance >= 4 then
                 if distance <= 50 and GetCharacterCondition(77, false) then
                     WalkTo(-641.2, 285.3, -138.7)
@@ -141,7 +200,7 @@ function LetsBuySomeStuff()
                 end
             end
         end
-        if Grade8DarkMatterCount < 99 and BuyDarkMatter then
+        if Grade8DarkMatterCount < BuyDarkMatterMinimum and BuyDarkMatterMinimum then
             if distance >= 4 then
                 if distance <= 50 and GetCharacterCondition(77, false) then
                     WalkTo(-641.2, 285.3, -138.7)
@@ -153,54 +212,21 @@ function LetsBuySomeStuff()
 
 
         local distance = GetDistanceToPoint(-641.2, 285.3, -138.7)
-        if BuyBait and distance <= 4 and DiademHoverwormCount < 99 then
-            while true do
-                local newDiademHoverwormCount = GetItemCount(30281)
-                if GetTargetName() ~= "Mender" then
-                    yield("/target Mender")
-                    yield("/wait 0.1")
-                elseif IsAddonVisible("SelectIconString") then
-                    yield("/pcall SelectIconString true 0")
-                elseif IsAddonVisible("SelectYesno") then
-                    yield("/pcall SelectYesno true 0")
-                    yield("/wait 0.1")
-                elseif DiademHoverwormCount < newDiademHoverwormCount then
-                    break
-                elseif IsAddonVisible("Shop") then
-                    yield("/pcall Shop true 0 6 99")
-                else
-                    yield("/interact")
-                end
-                yield("/wait 0.1")
-            end
+        if BuyBait and distance <= 4 and DiademHoverwormCount < BuyBaitMinimum then
+            local BuyAmount = MinimumBait - DiademHoverwormCount 
+            BuyPcall(30281, "Mender", "Shop", 0, 6, BuyAmount)
             LogInfo("[Debug]Bought Diadem Hoverworm.")
-        elseif not BuyBait and DiademHoverwormCount < 99 then
+        elseif not BuyBait and DiademHoverwormCount < BuyBaitMinimum then
             LogInfo("[Debug]BuyBait is False and Bait is running out, continue.")
         end
 
         yield("/wait 1") -- :D go ahed delete it don't cry to me if its broke tho.
 
-        if BuyDarkMatter and distance <= 4 and Grade8DarkMatterCount < 99 then
-            while true do
-                local newGrade8DarkMatterCount = GetItemCount(33916)
-                if GetTargetName() ~= "Mender" then
-                    yield("/target Mender")
-                    yield("/wait 0.1")
-                elseif IsAddonVisible("SelectIconString") then
-                    yield("/pcall SelectIconString true 0")
-                elseif IsAddonVisible("SelectYesno") then
-                    yield("/pcall SelectYesno true 0")
-                elseif Grade8DarkMatterCount < newGrade8DarkMatterCount then
-                    break
-                elseif IsAddonVisible("Shop") then
-                    yield("/pcall Shop true 0 14 99")
-                else
-                    yield("/interact")
-                end
-                yield("/wait 0.1")
-            end
+        if BuyDarkMatter and distance <= 4 and Grade8DarkMatterCount < BuyDarkMatterMinimum then
+            local BuyAmount = MinimumDarkMatter - Grade8DarkMatterCount 
+            BuyPcall(33916, "Mender", "Shop", 0, 14, BuyAmount)
             LogInfo("[Debug]Bought Grade8 DarkMatter.")
-        elseif not BuyDarkMatter and Grade8DarkMatterCount < 99 then
+        elseif not BuyDarkMatter and Grade8DarkMatterCount < BuyDarkMatterMinimum then
             LogInfo("[Debug]BuyDarkMatter is False and DarkMatter is running out, continue.")
         end
 
@@ -271,6 +297,32 @@ function MountAndFly()
     LogInfo("[MountAndFly] Completed")
 end
 
+function NpcRepairMenu(Name)
+    while true do
+        if not NeedsRepair(RepairAmount) then
+            break
+        elseif GetTargetName() ~= Name then
+            yield("/target "..Name)
+            yield("/wait 0.1")
+        elseif IsAddonVisible("SelectIconString") then
+            yield("/pcall SelectIconString true 1")
+        elseif IsAddonVisible("SelectYesno") then
+            yield("/pcall SelectYesno true 0")
+            yield("/wait 0.1")
+        elseif IsAddonVisible("Repair") then
+            yield("/pcall Repair true 0")
+        else
+            yield("/interact")
+        end
+        yield("/wait 0.1")
+    end
+    while IsAddonVisible("Repair") do
+        yield("/pcall Repair true -1")
+        yield("/wait 0.1")
+    end
+    LogInfo("[RepairNpc]Got Repaired by "..Name .." .")
+end
+
 function Repair()
     if NeedsRepair(RepairAmount) and SelfRepair then
         while not IsAddonVisible("Repair") do
@@ -300,9 +352,30 @@ function Repair()
         if IsAddonVisible("Repair") then
             yield("/pcall Repair true -1")
         end
-        PlayerTest()
-        LogInfo("[Repair] Completed")
     end
+
+    if NeedsRepair(RepairAmount) and NpcRepair then
+        if IsInZone(886) then
+            WalkTo(47, -16, 151)
+            if GetDistanceToPoint(47, -16, 151) <= 4 then
+                NpcRepairMenu("Eilonwy")
+            end
+        end
+        if IsInZone(939) then
+            if GetDistanceToPoint(-641.2, 285.3, -138.7) >= 4 then
+                if GetDistanceToPoint(-641.2, 285.3, -138.7) <= 50 and GetCharacterCondition(77, false) then
+                    WalkTo(-641.2, 285.3, -138.7)
+                else
+                    FlyTo(-641.2, 285.3, -138.7)
+                end
+            end
+            if GetDistanceToPoint(-641.2, 285.3, -138.7) <= 4 then
+                NpcRepairMenu("Mender")
+            end
+        end
+    end
+    PlayerTest()
+    LogInfo("[Repair] Completed")
 end
 
 function WalkTo(valuex, valuey, valuez)
@@ -373,25 +446,27 @@ function MoveToDiadem(RandomSelect)
             if not (GetCharacterCondition(4) and GetCharacterCondition(77)) then
                 MountAndFly()
             end
-            if not (PathIsRunning() or IsMoving()) then
-                PathfindAndMoveTo(X, Y, Z, true) -- Fly to spot
-                while PathfindInProgress() or PathIsRunning() and IsInZone(939) do
-                    yield("/wait 0.1")
-                    if not (GetCharacterCondition(4) and GetCharacterCondition(77)) then
-                        MountAndFly()
-                    end
+            PathfindAndMoveTo(X, Y, Z, true) -- Fly to spot
+            while GetDistanceToPoint(X, Y, Z) > 1 and IsInZone(939) do
+                yield("/wait 0.5")
+                if not (PathIsRunning() or IsMoving()) then
+                    PathfindAndMoveTo(X, Y, Z, true) -- Fly to spot
                 end
-                Dismount()
+                if not (GetCharacterCondition(4) and GetCharacterCondition(77)) then
+                    MountAndFly()
+                end
             end
+            Dismount()
         else
-            if not (PathIsRunning() or IsMoving()) then
-                PathfindAndMoveTo(X, Y, Z, false) -- Walk to spot
-                while PathfindInProgress() or PathIsRunning() and IsInZone(939) do
-                    yield("/wait 0.1")
+            while GetDistanceToPoint(X, Y, Z) > 1 and IsInZone(939) do
+                yield("/wait 0.5")
+                if not (PathIsRunning() or IsMoving()) then
+                    PathfindAndMoveTo(X, Y, Z, false) -- Walk to spot
                 end
-                Dismount()
             end
+            Dismount()
         end
+        yield("/wait 0.3")
         if RandomSelect == 0 then
             local oceanX, oceanY, oceanZ = X - 1.2, Y, Z - 1.2
             WalkTo(oceanX, oceanY, oceanZ)
@@ -407,9 +482,14 @@ function MoveToDiadem(RandomSelect)
 end
 
 function Bailout500Cast()
+    while GetCharacterCondition(6) do
+        yield("/ac Quit")
+        yield("/wait 1")
+    end
     MoveToDiadem(500)
     PlayerTest()
-    yield("/ahon")
+    yield("/wait 2")
+    yield("/ac Cast")
     yield("/wait 3")
     while GetCharacterCondition(6) do
         yield("/ac Quit")
@@ -419,33 +499,44 @@ function Bailout500Cast()
 end
 
 function Dofishing()
-    local MoveEveryMin = HowManyMinutes * 60
-    if IsInZone(939) then
-        SetAutoHookPreset()
-        if ForceAutoHookPreset then
-            yield("/wait 0.3")
-        yield("/bait Diadem Hoverworm")
-        end
-        yield("/wait 0.3")
-        yield("/ahon")
-        fishing_start_time = os.time()
-        yield("/ac Cast")
-        while fishing_start_time + MoveEveryMin > os.time() and IsInZone(939) do
-            if GetNodeText("_TextError", 1) ==
-                "The fish here have grown wise to your presence. You might have better luck in a new location..." and
-                IsAddonVisible("_TextError") then
-                Bailout500Cast()
+    if not GetCharacterCondition(4) then
+        local MoveEveryMin = HowManyMinutes * 60
+        NomNomDelish()
+        if IsInZone(939) then
+            SetAutoHookPreset()
+            if ForceAutoHookPreset then
+                yield("/wait 0.3")
+                yield("/bait Diadem Hoverworm")
             end
-            yield("/wait 5")
+            yield("/wait 0.3")
+            yield("/ahon")
+            fishing_start_time = os.time()
+            yield("/ac Cast")
+            yield("/wait 0.3")
+            while fishing_start_time + MoveEveryMin > os.time() and IsInZone(939) and GetItemCount(30281) > 0 do
+                if (GetNodeText("_ScreenText", 11, 8) ==
+                    "The fish here have grown wise to your presence. You might have better luck in a new location..." or
+                    GetNodeText("_ScreenText", 11, 8) ==
+                    "The fish sense something amiss. Perhaps it is time to try another location.") and
+                    IsNodeVisible("_ScreenText", 1, 40001) then
+                    Bailout500Cast()
+                    break
+                end
+                if not GetCharacterCondition(6) then
+                    yield("/ac Cast")
+                end
+                yield("/wait 2")
+            end
+            while GetCharacterCondition(6) do
+                yield("/ac Quit")
+                yield("/wait 1")
+            end
+            PlayerTest()
+            LogInfo("[Fishing] Completed")
         end
-        while GetCharacterCondition(6) do
-            yield("/ac Quit")
-            yield("/wait 1")
-        end
-        PlayerTest()
-        LogInfo("[Fishing] Completed")
     end
 end
+
 
 function WeGoIn()
     while IsInZone(886) do
@@ -487,6 +578,14 @@ end
 -- Setting up the some stuff
 CurrentJob = GetClassJobId()
 DiademHoverwormCount = GetItemCount(30281)
+PandoraSetFeatureState("Auto-Cordial",UseCordial)
+if not SelfRepair then
+    BuyDarkMatter = false
+end
+if NpcRepair and SelfRepair then
+    NpcRepair = false
+    yield("You can only select one repair setting. Setting Npc Repair false")
+end
 
 -- Set properties if they are not already set
 setPropertyIfNotSet("UseItemStructsVersion")
@@ -524,12 +623,10 @@ while true do
         WeGoIn()
         LetsBuySomeStuff()
         MoveToDiadem(0)
-        Repair()
-        NomNomDelish()
         Dofishing()
-        MoveToDiadem(1)
         Repair()
-        NomNomDelish()
+        LetsBuySomeStuff()
+        MoveToDiadem(1)
         Dofishing()
     else
         yield("/e Maybe go into firmament hee ?")
